462b74ca7fb798d0ee36b5b84969ddc98a00063d
Laura Reale <laaru1988@gmail.com>
2019-02-12 21:45:47 +0100

    add publisher can create articles feature scenarios, update basic_steps and assertions with new feature's happy path tests

--- features/step_definitions/assertions.rb
+++ features/step_definitions/assertions.rb
@@ -1,3 +1,8 @@
 Then("I should see {string}") do |expected_content|
     expect(page).to have_content expected_content
 end
+
+Then("I should be on {string} page") do |string|
+    expect(page).to have_title string
+end
+  
--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -7,3 +7,20 @@ end
 When("I visit the site") do
     visit '/'
 end
+
+Given("I visit the {string} page") do |page_name|
+    visit page_name
+end
+  
+When("I click {string} link") do |click_target|
+    click_link click_target
+end
+
+When("I fill in {string} with {string}") do |field, string|
+    fill_in field, with: string
+end
+
+When("I click {string} button") do |button_name|
+    click_button button_name
+end
+  
