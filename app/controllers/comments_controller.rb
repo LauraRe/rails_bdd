f3084be260bb3eb8e5cc969f7b6f85c836647ce6
Laura Reale <laaru1988@gmail.com>
2019-02-24 00:46:05 +0100

    create Comment model, add comment params private method to comments controller, add relation between article and comments, add scope to form in index, migrate db

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,12 @@
 class CommentsController < ApplicationController
   def create
+    article = Article.find(params[:article_id])
+    Comment.create(comment_params.merge!(article: article))
+  end
+
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:comment, :name)
   end
 end
